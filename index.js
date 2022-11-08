b2a0af1102fb6daa2bf1567b88d139c2e0304484
Galib6 <asadullahalgalib81@gmail.com>
2022-11-09 02:22:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const cors = require('cors');
+require("dotenv").config()
+const app = express();
+const port = process.env.PROT || 5000;
+
+// middlewere
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wjuepub.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const serviceCollection = client.db("photography").collection("services");
+        const reviewCollection = client.db("photography").collection("review");
+
+        app.get('/threeServices', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.sort({ _id: -1 }).limit(3).toArray()
+            res.send(services);
+        });
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.sort({ _id: -1 }).toArray()
+            res.send(services);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+        app.post('/services', async (req, res) => {
+            const order = req.body;
+            const result = await serviceCollection.insertOne(order);
+            res.send(result);
+        });
+        app.get('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id)
+            const query = { serviceId: id }
+            const cursor = reviewCollection.find(query);
+            const services = await cursor.sort({ _id: -1 }).toArray()
+            res.send(services);
+        });
+        app.post('/review', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        });
+    }
+
+    finally {
+
+    }
+
+}
+
+run().catch(err => console.log(err))
+
+console.log(process.env.DB_USER, process.env.DB_PASSWORD)
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Photopy Server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Photopy server running on ${port}`);
+})
